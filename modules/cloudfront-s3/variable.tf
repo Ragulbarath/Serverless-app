edc046f889342cc52a6ddf3fbb3513712c05e45e
mobin <mobthomas@gmail.com>
2024-03-01 09:22:14 +0000

    Newgit

---
+++ modules/cloudfront-s3/variable.tf
@@ -0,0 +1,10 @@
+variable "region_name" {
+ description = "name of the region"
+ default = "us-east-1a"
+}
+
+variable "s3_bucketname" {
+  description = "Name of S3 bucket"
+  default = "serverlessapptestbucket"
+}
+
